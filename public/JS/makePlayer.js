7df3feabe07a8d009db09258f5aebd7246b38903
Shresth <shresth.tiwary2019@vitstudent.ac.in>
2021-09-26 23:19:18 +0530

    Create makePlayer.js

---
+++ public/JS/makePlayer.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  const url = `https://playsuper.herokuapp.com/addPlayer`;
+
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Access-Control-Allow-Origin", "*");
+
+  $("#submit-player").on("click", function (e) {
+    e.preventDefault();
+
+    const name = $("#name").val();
+    const credits = $("#credits").val();
+    const ACS = $("#acs").val();
+    const KDRatio = $("#kdratio").val();
+    const teamName = $("#teamName").val();
+    const totalGamesPlayed = 0;
+
+    const data = {
+      name,
+      credits,
+      ACS,
+      KDRatio,
+      teamName,
+      totalGamesPlayed,
+    };
+
+    let json = JSON.stringify(data);
+    console.log(json);
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: json,
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      });
+  });
+});
