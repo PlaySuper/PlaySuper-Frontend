4d6e17a97e64f005af106f8a10a4687894e16ac6
saisukesh04 <sukesh.sai04@gmail.com>
2021-10-08 15:44:27 +0530

    Players fetching from team name

--- public/JS/makeMatch.js
+++ public/JS/makeMatch.js
@@ -1,14 +1,91 @@
+const playerFields = $(".players");
+const playerCredContainer = $(".player-cred-container");
+
+function createTeamPlayerDiv(teamA, teamB, length) {
+  let output = ``;
+
+  const fullPlayers = teamA.concat(teamB);
+  
+  for (let i = 0; i < length; i++) {
+    output += `
+      <input type="text" class="player" id="name${i+1}" value="${fullPlayers[i].name}" readonly/>
+      <input type="number" id="credits${i+1}" placeholder="Player ${i+1} Credits" min="0" required />`;
+  }
+
+  playerFields.empty();
+  playerFields.html(output);
+  playerCredContainer.show();
+}
+
 $(document).ready(function () {
   const url = `https://playsuper.herokuapp.com/addMatch`;
   $(".overlay").hide();
   $(".spinner").hide();
+  $("#submit-match").hide();
+  playerCredContainer.hide();
 
   let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Access-Control-Allow-Origin", "*");
 
   const subpoolFields = $(".subpool");
-  const playerFields = $(".players");
+
+  $('#fetch-players').on("click", function (e) {
+    e.preventDefault();
+
+    $(".overlay").show();
+    $(".spinner").show();
+
+    const teamA = $("#teamA").val();
+    const teamB = $("#teamB").val();
+
+    if (teamA && teamB){
+    const urlA = `https://playsuper.herokuapp.com/player/${teamA}`;
+    const urlB = `https://playsuper.herokuapp.com/player/${teamB}`;
+
+    let requestOptions = {
+      method: "GET",
+      headers: myHeaders
+    };
+
+    fetch(urlA, requestOptions)
+      .then((responseA) => responseA.json())
+      .then((resultA) => {
+        $(".overlay").hide();
+        $(".spinner").hide();
+
+        if (resultA.status == "failure")
+          swal("Oops something went wrong", "" + resultA.error, "error");
+        else {
+          fetch(urlB, requestOptions)
+            .then((responseB) => responseB.json())
+            .then((resultB) => {
+              $(".overlay").hide();
+              $(".spinner").hide();
+
+              if (resultB.status == "failure")
+                swal("Oops something went wrong", "" + resultB.error, "error");
+              else {
+                createTeamPlayerDiv(resultA.players, resultB.players, resultA.length + resultB.length);
+                $("#submit-match").show();
+              }
+            })
+            .catch((e2) => {
+              swal("Oops something went wrong", "" + e2, "error");
+              console.log(e2);
+            });
+        }
+      })
+      .catch((e1) => {
+        swal("Oops something went wrong", "" + e1, "error");
+        console.log(e1);
+      });
+    } else {
+      swal("Please fill the team fields", "Check the teamA and teamB fields");
+      $(".overlay").hide();
+        $(".spinner").hide();
+    }
+  });
 
   $("#submit-match").on("click", function (e) {
     $(".overlay").show();
